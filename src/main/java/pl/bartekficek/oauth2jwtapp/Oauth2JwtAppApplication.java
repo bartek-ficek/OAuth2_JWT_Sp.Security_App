3cb8f6be35f32f76b335766c1a96b4cc8c32b2af
Bartek Ficek <ficek.bartlomiej@gmail.com>
2020-12-30 11:19:53 +0100

    initial commit

---
+++ src/main/java/pl/bartekficek/oauth2jwtapp/Oauth2JwtAppApplication.java
@@ -0,0 +1,13 @@
+package pl.bartekficek.oauth2jwtapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2JwtAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2JwtAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/bartekficek/oauth2jwtapp/Oauth2JwtAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bartekficek.oauth2jwtapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2JwtAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
